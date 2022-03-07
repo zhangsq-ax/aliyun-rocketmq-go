4fe78c14b98cfebadcfbfe095da2f9e93112abf8
张世琦 <zhangsq@zhangshiqideMacBook-Pro.local>
2022-03-07 12:38:51 +0800

    feat: initialize project

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/zhangsq-ax/aliyun-rocketmq-go
+
+go 1.17
+
+require (
+	github.com/apache/rocketmq-client-go/v2 v2.1.0
+	github.com/zhangsq-ax/logs v1.0.0
+)
+
+require (
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/golang/mock v1.3.1 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/sirupsen/logrus v1.4.1 // indirect
+	github.com/tidwall/gjson v1.2.1 // indirect
+	github.com/tidwall/match v1.0.1 // indirect
+	github.com/tidwall/pretty v0.0.0-20190325153808-1166b9ac2b65 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	stathat.com/c/consistent v1.0.0 // indirect
+)
---
+++ rocketmq.go
@@ -0,0 +1,169 @@
+package aliyun_rocketmq_go
+
+import (
+	"context"
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/consumer"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"github.com/apache/rocketmq-client-go/v2/producer"
+	"github.com/zhangsq-ax/logs"
+	"go.uber.org/zap"
+)
+
+type RocketHelperOptions struct {
+	Endpoint        string // Aliyun RocketMQ 服务接入点
+	InstanceId      string // Aliyun RocketMQ 服务实例标识
+	GroupId         string // 客户端 Group 标识
+	AccessKeyId     string
+	AccessKeySecret string
+}
+
+func (opts *RocketHelperOptions) GetCredentials() primitive.Credentials {
+	return primitive.Credentials{
+		AccessKey: opts.AccessKeyId,
+		SecretKey: opts.AccessKeySecret,
+	}
+}
+
+type RocketHelper struct {
+	opts *RocketHelperOptions
+}
+
+func NewRocketHelper(opts *RocketHelperOptions) *RocketHelper {
+	return &RocketHelper{
+		opts: opts,
+	}
+}
+
+func (rh *RocketHelper) CreatePushConsumer() (rocketmq.PushConsumer, error) {
+	opts := rh.opts
+	logs.Infow("create-rocketmq-push-consumer",
+		zap.Reflect("options", map[string]interface{}{
+			"endpoint":   opts.Endpoint,
+			"instanceId": opts.InstanceId,
+			"groupId":    opts.GroupId,
+		}),
+	)
+	return rocketmq.NewPushConsumer(
+		consumer.WithNameServer([]string{opts.Endpoint}),
+		consumer.WithNamespace(opts.InstanceId),
+		consumer.WithInstance(opts.InstanceId),
+		consumer.WithGroupName(opts.GroupId),
+		consumer.WithCredentials(opts.GetCredentials()),
+		consumer.WithConsumerModel(consumer.Clustering),
+		consumer.WithConsumeFromWhere(consumer.ConsumeFromLastOffset),
+		consumer.WithConsumerOrder(true),
+	)
+}
+
+var pushConsumer rocketmq.PushConsumer
+
+func (rh *RocketHelper) getPushConsumer() (rocketmq.PushConsumer, error) {
+	if pushConsumer == nil {
+		var err error
+		pushConsumer, err = rh.CreatePushConsumer()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return pushConsumer, nil
+}
+
+func (rh *RocketHelper) PushConsumeByConsumer(c rocketmq.PushConsumer, topic string, onMessage func(*primitive.MessageExt) error) error {
+	logs.Infow("subscribe-rocketmq",
+		zap.String("topic", topic),
+	)
+	err := c.Subscribe(topic, consumer.MessageSelector{}, func(ctx context.Context, messages ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+		for _, msg := range messages {
+			logs.Debugw("received-rocketmq-message",
+				zap.Reflect("summary", map[string]interface{}{
+					"messageId":      msg.MsgId,
+					"topic":          msg.Topic,
+					"queueOffset":    msg.QueueOffset,
+					"tags":           msg.GetTags(),
+					"keys":           msg.GetKeys(),
+					"properties":     msg.GetProperties(),
+					"regionId":       msg.GetRegionID(),
+					"reconsumeTimes": msg.ReconsumeTimes,
+				}),
+			)
+			err := onMessage(msg)
+			if err != nil {
+				logs.Warnw("process-message-failed", zap.Error(err))
+				return consumer.ConsumeRetryLater, err
+			}
+		}
+		return consumer.ConsumeSuccess, nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return c.Start()
+}
+
+func (rh *RocketHelper) PushConsume(topic string, onMessage func(*primitive.MessageExt) error) error {
+	c, err := rh.getPushConsumer()
+	if err != nil {
+		return err
+	}
+	return rh.PushConsumeByConsumer(c, topic, onMessage)
+}
+
+func (rh *RocketHelper) NewProducer() (rocketmq.Producer, error) {
+	opts := rh.opts
+	logs.Infow("create-rocketmq-producer",
+		zap.String("endpoint", opts.Endpoint),
+		zap.String("instanceId", opts.InstanceId),
+		zap.String("groupId", opts.GroupId),
+	)
+	credentials := opts.GetCredentials()
+	return rocketmq.NewProducer(
+		producer.WithNameServer([]string{opts.Endpoint}),
+		producer.WithNamespace(opts.InstanceId),
+		producer.WithInstanceName(opts.InstanceId),
+		producer.WithGroupName(opts.GroupId),
+		producer.WithCredentials(credentials),
+		producer.WithTrace(&primitive.TraceConfig{
+			GroupName:    opts.GroupId,
+			Access:       primitive.Cloud,
+			NamesrvAddrs: []string{opts.Endpoint},
+			Credentials:  credentials,
+		}),
+	)
+}
+
+func (rh *RocketHelper) CreatePublicMessage(topic string, body []byte, tag string, keys []string, properties map[string]string) *primitive.Message {
+	msg := &primitive.Message{
+		Topic: topic,
+		Body:  body,
+	}
+	msg.WithProperties(properties)
+
+	return msg.WithTag(tag).WithKeys(keys)
+}
+
+var p rocketmq.Producer
+
+func (rh *RocketHelper) getProducer() (rocketmq.Producer, error) {
+	if p == nil {
+		var err error
+		p, err = rh.NewProducer()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return p, nil
+}
+
+func (rh *RocketHelper) SendMessageByProducer(ctx context.Context, p rocketmq.Producer, msg *primitive.Message) (*primitive.SendResult, error) {
+	return p.SendSync(ctx, msg)
+}
+
+func (rh *RocketHelper) SendMessage(ctx context.Context, msg *primitive.Message) (*primitive.SendResult, error) {
+	p, err := rh.getProducer()
+	if err != nil {
+		return nil, err
+	}
+	return rh.SendMessageByProducer(ctx, p, msg)
+}
